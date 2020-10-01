b43ee54b94493afadbefc96cf80e120ca1f4f7a4
Connor Wulf <connor.m.wulf@gmail.com>
2020-10-01 14:21:13 -0500

    defined sayHello function

--- code.js
+++ code.js
@@ -1,4 +1,14 @@
-// helloWorld function 
+ // helloWorld function
+// const helloWorld = function() {
+//     return "Hello, World!";
+// }
+
 function helloWorld() {
     return "Hello, World!";
 }
+ 
+ //define sayHello function
+ 
+ function sayHello() {
+
+ }
